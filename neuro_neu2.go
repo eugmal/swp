c8cdd5945e2f977da3f937d88b89630c3d01615d
eugen <eugen.mal@pm.me>
2022-04-03 15:16:17 +0200

    kommentare zu den Funktionen. hinzufuegen vom Bias. funktion fuer allgemeinen Layer Output

--- neuro_neu2.go
+++ neuro_neu2.go
@@ -36,40 +36,84 @@ func weights_init(anzahl_input_neuronen, anzahl_neuronen_akt_layer int) mat.Dens
 	a := mat.NewDense(anzahl_input_neuronen, anzahl_neuronen_akt_layer, data)
 	return *a
 }
-
-//zufallswerte der biases initialisieren
 func biases_init(anzahl_neuronen_akt_layer int) mat.Dense {
 	data := make([]float64, (anzahl_neuronen_akt_layer))
 	for i := range data {
 		data[i] = rand.NormFloat64()
 	}
-	a := mat.NewDense(anzahl_neuronen_akt_layer, 1, data)
+	a := mat.NewDense(1, anzahl_neuronen_akt_layer, data)
 	return *a
 
 }
 
 /*
-func mul (passenderen Namen waehlen!)
-berechnet den output des ersten hiddenLayers (hier nur beispielhaft. Es fehlt bias)
+>> func createLayerOutput <<
+berechnet den output des ersten hiddenLayers
 da x (die matrix mit dem inputbild) die dimensionen 28*28,1 hat muss Transponiert werden. bei den Parameter anzahl_nodes_des_hiddenlayer muss die Anzahl der Nodes des layers in den die daten reingehen angegeben werden. Der Output ist eine Matrix mit den Dimensionen 1, Anzahl der Nodes.(Nodes sind also gewissermassen die Spalten). Damit sind die Output-Daten als input fuer den naechsten Layer schon in der richtigen Form und es muss dann nichtmehr transponiert werden.
 */
-func mul(x, y *mat.Dense, anzahl_nodes_des_hiddenLayer int) *mat.Dense {
+func create1HiddenLayerOutput(input, weights_matrix, bias_matrix *mat.Dense, anzahl_nodes_des_hiddenLayer int) *mat.Dense {
+	// Matritzen-Multiplikation der Input-Matrix mit der Weights_Matrix des aktuellen Layers.
 	output := mat.NewDense(1, anzahl_nodes_des_hiddenLayer, nil)
-	output.Mul(x.T(), y)
+	output.Mul(input.T(), weights_matrix)
+	//der entsprechende Bias wird zu den Ergebnissen addiert.
+	output_bias := mat.NewDense(1, anzahl_nodes_des_hiddenLayer, nil)
+	output_bias.Add(bias_matrix, output)
+
 	//hilfsfunktion fuer die Apply-Methode
 	applySigmoid := func(_, _ int, v float64) float64 { return sigmoid(v) }
+	//sigmoid wird auf jedes Ergebnis angewandt.
 	output_sigmoid := mat.NewDense(1, anzahl_nodes_des_hiddenLayer, nil)
 	//wendet die sigmoid-Funktion auf jedes Ergebnis der Multiplikation an.
-	output_sigmoid.Apply(applySigmoid, output)
+	output_sigmoid.Apply(applySigmoid, output_bias)
+	return output_sigmoid
+}
+
+// im Prinzip das gleiche wie create1HiddenLayerOutput aber ohne den Transpose der Inputdaten
+func createGeneralLayerOutput(input, weights_matrix, bias_matrix *mat.Dense, anzahl_nodes_des_hiddenLayer int) *mat.Dense {
+	// Matritzen-Multiplikation der Input-Matrix mit der Weights_Matrix des aktuellen Layers.
+	output := mat.NewDense(1, anzahl_nodes_des_hiddenLayer, nil)
+	output.Mul(input, weights_matrix)
+	//der entsprechende Bias wird zu den Ergebnissen addiert.
+	output_bias := mat.NewDense(1, anzahl_nodes_des_hiddenLayer, nil)
+	output_bias.Add(bias_matrix, output)
+
+	//hilfsfunktion fuer die Apply-Methode
+	applySigmoid := func(_, _ int, v float64) float64 { return sigmoid(v) }
+	//sigmoid wird auf jedes Ergebnis angewandt.
+	output_sigmoid := mat.NewDense(1, anzahl_nodes_des_hiddenLayer, nil)
+	//wendet die sigmoid-Funktion auf jedes Ergebnis der Multiplikation an.
+	output_sigmoid.Apply(applySigmoid, output_bias)
 	return output_sigmoid
 }
 
 func main() {
-
+	//--------------------------------------------------------------
+	// bestimmen der Layer-Variablen
 	var hidden1 hidden_layer
+	var hidden2 hidden_layer
+	var output_final hidden_layer
+
+	//--------------------------------------------------------------
+
+	// initialisieren der Layer weights und biases
 	hidden1.neuronen = 16
 	hidden1.weights_matrix = weights_init(28*28, 16)
+	hidden1.biases = biases_init(16)
+
+	hidden2.neuronen = 16
+	hidden2.weights_matrix = weights_init(16, 16)
+	hidden2.biases = biases_init(16)
+
+	output_final.neuronen = 10
+	output_final.weights_matrix = weights_init(16, 10)
+	output_final.biases = biases_init(10)
+
+	// -------------------------------------------------------------
+
+	// laden der datensets
 	trainingData, _ := mnistLoad.Laden("/Users/eugen/coding/lwb/mnistzip")
+
+	// --------------------------------------------------------------
 	fmt.Println(trainingData.Bilder[1])
 	//fmt.Println(trainingData.Labels[1])
 	einBild_Bild := trainingData.Bilder[1]
@@ -80,7 +124,10 @@ func main() {
 
 	einBild := mat.NewDense(28*28, 1, dataBild)
 	fmt.Println(einBild)
-	//hiddenMatrix := mat.NewDense(28*28, 16, nil)
+	output_layerH1 := create1HiddenLayerOutput(einBild, &hidden1.weights_matrix, &hidden1.biases, 16)
 
-	fmt.Println(mul(einBild, &hidden1.weights_matrix, 16))
+	//fmt.Println(create1HiddenLayerOutput(einBild, &hidden1.weights_matrix, &hidden1.biases, 16))
+	output_layerH2 := createGeneralLayerOutput(output_layerH1, &hidden2.weights_matrix, &hidden2.biases, 16)
+
+	fmt.Println(createGeneralLayerOutput(output_layerH2, &output_final.weights_matrix, &output_final.biases, 10))
 }
