083b79d6879ea1e2422fe5592b16fc3efa7c9d7c
herr.laudes@gmail.com <herr.laudes@gmail.com>
2022-04-25 10:19:28 +0200

    neue Darstellung

--- ldarstellung/ldarstellungenImpl.go
+++ ldarstellung/ldarstellungenImpl.go
@@ -6,27 +6,33 @@
 package ldarstellungen
 
 import (
-	//"fmt"
-	 //"./lmodell"
+	"fmt"
+	// ".swp//lmodell"
 	 "gfx"
 	//"github.com/petjanzen/lmodelle"
 )
 
 
 //const flip float64 = 0.0167  // Konstante frei wählbar. Hier gewählt nach Auswertung des data.w-Slices
-const vergroesserung uint16 = 20 //Vergößert die 28*28-Pixel bilder
+const vergroesserung uint16 = 20 //Vergößert die 28*28-Pixel Bilder
 
 
 
-func Umwandeln (slice []float64, flip float64) ([]uint8){
+
+func Umwandeln (slice []float64, bWert float64) ([]uint8){
 	var erg []uint8 = make ([]uint8,len(slice))  //Variable für das Ergebnis erstellen
 	
 	for i,w := range slice {
-		if w > flip {
+		w=w-bWert //hier wird der bWert (bias) neutralisiert, um die Daten nicht zu verfälschen
+		fmt.Println (w)
+		if w > 0 {
 			erg[i] = uint8(1)  //alles. was größer ist als die Konstante "flip" wird zur 1...
+		} else if w < 0 {
+			erg[i] = uint8(2) 
 		} else {
 			erg[i] = uint8(0) //alles, was kleiner ist als die Konstante "flip"  wird zur 0.  
 		}
+	
 			
 		
 	}
@@ -46,6 +52,9 @@ func Darstellen (slice []uint8) {  					//für die gfx Darstellung eines Binärc
 			gfx.Vollrechteck (x,y, vergroesserung, vergroesserung)	
 		} else if w==0{  							//Wenn 0 ausgelesen wird, zeichne Quardat mit Seitenlänge vergroesserung in türkis
 			gfx.Stiftfarbe (0,255,255)
+			gfx.Vollrechteck (x,y, vergroesserung,vergroesserung)
+		} else if w==2{  							//Wenn 0 ausgelesen wird, zeichne Quardat mit Seitenlänge vergroesserung in türkis
+			gfx.Stiftfarbe (255,0,0)
 			gfx.Vollrechteck (x,y, vergroesserung,vergroesserung)
 		}
 		x = (x+vergroesserung) % (28*vergroesserung)  //Neusetzung von x
